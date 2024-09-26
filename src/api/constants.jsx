dce0bcabd4210dea5042aa3d35232d6af0a5693e
Ankit sharma <ankit.sharma09547@gmail.com>
2024-09-27 00:32:46 +0530

    fix

--- src/api/constants.jsx
+++ src/api/constants.jsx
@@ -1,33 +1,33 @@
-const apiEndpoint = import.meta.env.VITE_APP_API_URL;
+// const apiEndpoint = import.meta.env.VITE_APP_API_URL;
 export const apiConstants = [
   {
     actionType: "USER_LOGIN",
-    endpoint: `${apiEndpoint}/user/login`,
+    endpoint: `/user/login`,
     method: "POST",
   },
   {
     actionType: "USER_SIGNUP",
-    endpoint: `${apiEndpoint}/user/register`,
+    endpoint: `/user/register`,
     method: "POST",
   },
   {
     actionType: "USER_LOG_OUT",
-    endpoint: `${apiEndpoint}/user/logout`,
+    endpoint: `/user/logout`,
     method: "POST",
   },
   {
     actionType: "USER_FORGOT_PASSWORD",
-    endpoint: `${apiEndpoint}/user/forgot-password`,
+    endpoint: `/user/forgot-password`,
     method: "POST",
   },
   {
     actionType: "USER_PASSWORD_RESET",
-    endpoint: `${apiEndpoint}/user/reset-password/:resetToken`,
+    endpoint: `/user/reset-password/:resetToken`,
     method: "POST",
   },
   {
     actionType: "USER_DETAILS",
-    endpoint: `${apiEndpoint}/user`,
+    endpoint: `/user`,
     method: "GET",
   },
 ];
